49d738f65967301997dca84478ddfe84cdc9adc2
Dan Tenenbaum <dtenenba@fhcrc.org>
2013-02-25 17:44:20 +0000

    fix references to defunct is.real() and replace with is.double()
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/tigre@73727 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/miscFunctions.R
+++ R/miscFunctions.R
@@ -396,10 +396,10 @@ modelLogLikelihood <- function (model) {
 
 
 .complexLog <- function (x) {
-  if ( is.real(x) & x>0 ) {
+  if ( is.double(x) & x>0 ) {
       y <- log(x)
   } else {
-      if ( is.real(x) & x<0 )
+      if ( is.double(x) & x<0 )
           warning("Log of negative real number, using complex log!")
       y <- log(x+0i)
   }
